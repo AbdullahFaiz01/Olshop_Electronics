1c7fb42dbca95939b154efe45c8dff2c210f5db6
AbdullahFaiz01 <105841117724@student.unismuh.ac.id>
2025-11-26 00:36:18 +0800

    beruna sendmail

--- sendMail.js
+++ sendMail.js
@@ -1,6 +1,4 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
 
 export default async function sendMail({ to, subject, text }) {
   const transporter = nodemailer.createTransport({
